09e3ec4757af1b4eb892466491d7c68e175f4146
prius06 <canonmp500priinter@yahoo.co.jp>
2021-03-27 22:25:18 +0900

    問題数増やしてみたけど、最初からにはリロードするしかない状態だよ

--- quize/app.js
+++ quize/app.js
@@ -1,14 +1,34 @@
-const question = "ゲーム機で夫も売れたゲーム機は次の内どれ？";
-const answers = [
-	'スーパーファミコン',
-	'プレイステーション２',
-	'ニンテンドーswitch',
-	'ニンテンドーDS'];
-	const correct  = 'ニンテンドーDS';
+const quiz = [
+	{
+		question : 'ゲーム機で夫も売れたゲーム機は次の内どれ？',
+		answers : [
+		'スーパーファミコン',
+		'プレイステーション２',
+		'ニンテンドーswitch',
+		'ニンテンドーDS'],
+		correct : 'ニンテンドーDS'
+},{
+	question : '室内競技はどれ？',
+	answers : [
+		'野球',
+		'サッカー',
+		'バスケットボール',
+		'陸上'],
+		correct : 'バスケットボール'
+},{
+	question : '女の子に人気の職業はどれ？',
+	answers : [
+		'お花屋さん',
+		'ケーキ屋さん',
+		'パイロット',
+		'警察官'],
+		correct : 'ケーキ屋さん'
+}
+];
+	const quizLength = quiz.length;
+	let quizIndex = 0;
+	let score = 0;
 
-	console.log();
-
-	document.getElementById('js-question').textContent = question;
 
 	const $button = document.getElementsByTagName('button')
 
@@ -16,19 +36,29 @@ const answers = [
 		let buttonIndex = 0;
 		let buttonLength = $button.length;
 		while(buttonIndex < buttonLength){
-			$button[buttonIndex].textContent = answers[buttonIndex];
+	document.getElementById('js-question').textContent = quiz[quizIndex].question;
+	$button[buttonIndex].textContent = quiz[quizIndex ].answers[buttonIndex];
 			buttonIndex++;
 		}
 	};
 	setUpQuize();
 
 	const clickHandler = (e) => {
-		if(correct === e.target.textContent){
+		if(quiz[quizIndex].correct === e.target.textContent){
 			window.alert('正解！');
+			score++;
 		} else{
 			window.alert('不正解！');
 		}
-	}
+
+		quizIndex++;
+
+		if(quizIndex < quizLength){
+			setUpQuize();
+		}else{
+			window.alert('終了！あなたの正解数は' + score + '/' + quizLength + 'です');
+		}
+	};
 
 	let handlerIndex = 0;
 	while (handlerIndex < $button.length){

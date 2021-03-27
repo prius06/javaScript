f962927346274f7b4a0e397a698ad4a48598e7a0
prius06 <canonmp500priinter@yahoo.co.jp>
2021-03-27 18:20:24 +0900

    リファクタリングって大変だけどスッキリするね

--- quize/app.js
+++ quize/app.js
@@ -12,36 +12,29 @@ const answers = [
 
 	const $button = document.getElementsByTagName('button')
 
-	$button[0].textContent = answers[0];
-	$button[1].textContent = answers[1];
-	$button[2].textContent = answers[2];
-	$button[3].textContent = answers[3];
+	const setUpQuize = () => {
+		let buttonIndex = 0;
+		let buttonLength = $button.length;
+		while(buttonIndex < buttonLength){
+			$button[buttonIndex].textContent = answers[buttonIndex];
+			buttonIndex++;
+		}
+	};
+	setUpQuize();
 
-	$button[0].addEventListener('click',() => {
-		if(correct === $button[0].textContent){
+	const clickHandler = (e) => {
+		if(correct === e.target.textContent){
 			window.alert('正解！');
 		} else{
 			window.alert('不正解！');
 		}
-	});
-	$button[1].addEventListener('click',() => {
-		if(correct === $button[1].textContent){
-			window.alert('正解！');
-		} else{
-			window.alert('不正解！');
-		}
-	});
-	$button[2].addEventListener('click',() => {
-		if(correct === $button[2].textContent){
-			window.alert('正解！');
-		} else{
-			window.alert('不正解！');
-		}
-	});
-	$button[3].addEventListener('click',() => {
-		if(correct === $button[3].textContent){
-			window.alert('正解！');
-		} else{
-			window.alert('不正解！');
-		}
-	});
+	}
+
+	let handlerIndex = 0;
+	while (handlerIndex < $button.length){
+		$button[handlerIndex].addEventListener('click',(e) => {
+			clickHandler(e);
+		});
+		handlerIndex++;
+	}
+	
